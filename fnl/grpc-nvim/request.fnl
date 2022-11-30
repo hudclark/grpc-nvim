2031e199f402040fc0f020e0d57df4df94c9a4b7
hudclark <hudclark@umich.edu>
2022-11-30 07:56:22 -0500

    Validate cursor is within request bounds

--- fnl/grpc-nvim/request.fnl
+++ fnl/grpc-nvim/request.fnl
@@ -3,13 +3,10 @@
   (vim.fn.search "^grpc" "cbn" 1))
 
 (fn read-request-args [pos]
-  (let [line (vim.fn.getline pos)
-	parts (vim.fn.split line " ")]
-    (assert (> (length parts) 1))
-    (assert (= (string.lower (. parts 1)) "grpc"))
-    ;; Drop the 'grpc' prefix from the line
-    (table.remove parts 1)
-    parts))
+  "Returns space-separated arguments on a line starting with 'grpc'"
+  (match (vim.fn.split (vim.fn.getline pos) " ")
+    (where [cmd & args] (= (string.lower cmd) "grpc")) args
+    _ nil))
 
 (fn read-request-data [pos]
   "Reads JSON data starting from pos"
@@ -24,15 +21,19 @@
 				    [line])))))]
     (read pos)))
 
-
 (fn request-from-cursor []
-  (let [start (find-request-start)]
-    (if (> start 0)
-	(let [data (read-request-data (+ start 1))
-	      end (+ start (if data (length data) 0))]
-	{: start : end
-	:args (read-request-args start)
-	:data data})
-	nil)))
+  "Returns a request if the cursor is within its bounds, nil otherwise"
+  (match (find-request-start)
+    ;; Failed to find the start of a request
+    -1 nil
+    start (let [args (read-request-args start)
+		data (read-request-data (+ start 1))
+	        end (+ start (if data (length data) 0))
+		cursor (. (vim.fn.getcurpos) 2)]
+	    ;; Ensure that the cursor is actually within the bounds
+	    ;; of the request.
+	    (if (<= cursor end)
+		{: start : end : args : data}
+		nil))))
 
 {: request-from-cursor}
--- lua/grpc-nvim/request.lua
+++ lua/grpc-nvim/request.lua
@@ -2,26 +2,36 @@ local function find_request_start()
   return vim.fn.search("^grpc", "cbn", 1)
 end
 local function read_request_args(pos)
-  local line = vim.fn.getline(pos)
-  local parts = vim.fn.split(line, " ")
-  assert((#parts > 1))
-  assert((string.lower(parts[1]) == "grpc"))
-  table.remove(parts, 1)
-  return parts
+  local _1_ = vim.fn.split(vim.fn.getline(pos), " ")
+  local function _2_()
+    local cmd = (_1_)[1]
+    local args = {select(2, (table.unpack or _G.unpack)(_1_))}
+    return (string.lower(cmd) == "grpc")
+  end
+  if (((_G.type(_1_) == "table") and (nil ~= (_1_)[1])) and _2_()) then
+    local cmd = (_1_)[1]
+    local args = {select(2, (table.unpack or _G.unpack)(_1_))}
+    return args
+  elseif true then
+    local _ = _1_
+    return nil
+  else
+    return nil
+  end
 end
 local function read_request_data(pos)
   local stop = vim.fn.line("$")
   local read
-  local function _1_(pos0, lines)
+  local function _4_(pos0, lines)
     if (pos0 > stop) then
       return lines
     else
-      local _2_ = vim.fn.getline(pos0)
-      if (_2_ == "") then
+      local _5_ = vim.fn.getline(pos0)
+      if (_5_ == "") then
         return lines
-      elseif (nil ~= _2_) then
-        local line = _2_
-        local function _3_()
+      elseif (nil ~= _5_) then
+        local line = _5_
+        local function _6_()
           if lines then
             table.insert(lines, line)
             return lines
@@ -29,29 +39,38 @@ local function read_request_data(pos)
             return {line}
           end
         end
-        return read((pos0 + 1), _3_())
+        return read((pos0 + 1), _6_())
       else
         return nil
       end
     end
   end
-  read = _1_
+  read = _4_
   return read(pos)
 end
 local function request_from_cursor()
-  local start = find_request_start()
-  if (start > 0) then
+  local _9_ = find_request_start()
+  if (_9_ == -1) then
+    return nil
+  elseif (nil ~= _9_) then
+    local start = _9_
+    local args = read_request_args(start)
     local data = read_request_data((start + 1))
     local _end
-    local function _6_()
+    local function _10_()
       if data then
         return #data
       else
         return 0
       end
     end
-    _end = (start + _6_())
-    return {start = start, ["end"] = _end, args = read_request_args(start), data = data}
+    _end = (start + _10_())
+    local cursor = vim.fn.getcurpos()[2]
+    if (cursor <= _end) then
+      return {start = start, ["end"] = _end, args = args, data = data}
+    else
+      return nil
+    end
   else
     return nil
   end
