f3f8553c0c735e8caf998f30ee18dbdc9ee84203
hudclark <hudclark@umich.edu>
2022-12-04 11:01:38 -0500

    Adding loading spinner and result extmark

--- fnl/grpc-nvim/grpc.fnl
+++ fnl/grpc-nvim/grpc.fnl
@@ -6,18 +6,64 @@
 ;; The buffer we'll use to hold GRPC responses.
 (local buffer-name :grpc_nvim_results)
 
-;; Returns a callback that appends a job's output to the
-;; given buffer.
+(local spinner-frames ["⠋"
+                       "⠙"
+                       "⠹"
+                       "⠸"
+                       "⠼"
+                       "⠴"
+                       "⠦"
+                       "⠧"
+                       "⠇"
+                       "⠏"])
+
+(local namespace (api.nvim_create_namespace :grpc-nvim))
+
 (fn append-to-buf-cb [buf]
+  "Returns a fn that appends the second argument to buf"
   #(api.nvim_buf_set_lines buf -1 -1 false [$2]))
+
+(fn draw-spinner [buf text id]
+  "Draw the spinner, returning the extmark id"
+  (api.nvim_buf_set_extmark buf namespace 0 0
+                            (vim.tbl_extend :force
+                                            {:virt_text_pos :eol
+                                             :hl_mode :combine
+                                             :virt_text [text]}
+                                            (if id {: id} {}))))
+
+(fn start-spinner [buf]
+  (let [extmark (draw-spinner buf [(. spinner-frames 1) :Comment])
+        timer (vim.loop.new_timer)]
+    (var frame 1)
+    (timer:start 0 150
+                 (vim.schedule_wrap (fn []
+                                      (set frame
+                                           (if (< frame (length spinner-frames))
+                                               (+ frame 1)
+                                               1))
+                                      (draw-spinner buf
+                                                    [(. spinner-frames frame)
+                                                     :Comment]
+                                                    extmark)
+                                      (vim.cmd :redraw))))
+    {: extmark : timer : frame}))
+
+(fn spinner-finish [buf exit-code spinner]
+  (spinner.timer:close)
+  (draw-spinner buf (if (= 0 exit-code) ["✓" :Comment] ["✗" :WarningMsg])
+                spinner.extmark))
 
 (fn execute-request [request buf]
   (let [cb (vim.schedule_wrap (append-to-buf-cb buf))
         data-args (if request.data [:-d (table.concat request.data "\n")] [])
+        spinner (start-spinner buf)
         job (Job:new {:command :grpcurl
                       :args (vim.fn.extend data-args request.args)
                       :on_stdout cb
-                      :on_stderr cb})]
+                      :on_stderr cb
+                      :on_exit (vim.schedule_wrap #(spinner-finish buf $2
+                                                                   spinner))})]
     ;; Open the buffer in a split, if not already open
     (when (= -1 (vim.fn.bufwinnr buf))
       (vim.cmd (.. "vert sb" buf)))
--- lua/grpc-nvim/grpc.lua
+++ lua/grpc-nvim/grpc.lua
@@ -3,11 +3,49 @@ local buffer = require("grpc-nvim.buffer")
 local Job = require("plenary.job")
 local request = require("grpc-nvim.request")
 local buffer_name = "grpc_nvim_results"
+local spinner_frames = {"\226\160\139", "\226\160\153", "\226\160\185", "\226\160\184", "\226\160\188", "\226\160\180", "\226\160\166", "\226\160\167", "\226\160\135", "\226\160\143"}
+local namespace = api.nvim_create_namespace("grpc-nvim")
 local function append_to_buf_cb(buf)
   local function _1_(_241, _242)
     return api.nvim_buf_set_lines(buf, -1, -1, false, {_242})
   end
   return _1_
+end
+local function draw_spinner(buf, text, id)
+  local function _2_()
+    if id then
+      return {id = id}
+    else
+      return {}
+    end
+  end
+  return api.nvim_buf_set_extmark(buf, namespace, 0, 0, vim.tbl_extend("force", {virt_text_pos = "eol", hl_mode = "combine", virt_text = {text}}, _2_()))
+end
+local function start_spinner(buf)
+  local extmark = draw_spinner(buf, {spinner_frames[1], "Comment"})
+  local timer = vim.loop.new_timer()
+  local frame = 1
+  local function _3_()
+    if (frame < #spinner_frames) then
+      frame = (frame + 1)
+    else
+      frame = 1
+    end
+    draw_spinner(buf, {spinner_frames[frame], "Comment"}, extmark)
+    return vim.cmd("redraw")
+  end
+  timer:start(0, 150, vim.schedule_wrap(_3_))
+  return {extmark = extmark, timer = timer, frame = frame}
+end
+local function spinner_finish(buf, exit_code, spinner)
+  do end (spinner.timer):close()
+  local _5_
+  if (0 == exit_code) then
+    _5_ = {"\226\156\147", "Comment"}
+  else
+    _5_ = {"\226\156\151", "WarningMsg"}
+  end
+  return draw_spinner(buf, _5_, spinner.extmark)
 end
 local function execute_request(request0, buf)
   local cb = vim.schedule_wrap(append_to_buf_cb(buf))
@@ -17,7 +55,12 @@ local function execute_request(request0, buf)
   else
     data_args = {}
   end
-  local job = Job:new({command = "grpcurl", args = vim.fn.extend(data_args, request0.args), on_stdout = cb, on_stderr = cb})
+  local spinner = start_spinner(buf)
+  local job
+  local function _8_(_241, _242)
+    return spinner_finish(buf, _242, spinner)
+  end
+  job = Job:new({command = "grpcurl", args = vim.fn.extend(data_args, request0.args), on_stdout = cb, on_stderr = cb, on_exit = vim.schedule_wrap(_8_)})
   if (-1 == vim.fn.bufwinnr(buf)) then
     vim.cmd(("vert sb" .. buf))
   else
@@ -26,24 +69,24 @@ local function execute_request(request0, buf)
   return job:start()
 end
 local function make_result_header(request0)
-  local function _4_(_241)
+  local function _10_(_241)
     return ("// " .. _241)
   end
-  local function _5_()
+  local function _11_()
     if request0.data then
       return request0.data
     else
       return {}
     end
   end
-  return vim.fn.extend({("// grpcurl " .. table.concat(request0.args, " "))}, vim.tbl_map(_4_, _5_()))
+  return vim.fn.extend({("// grpcurl " .. table.concat(request0.args, " "))}, vim.tbl_map(_10_, _11_()))
 end
 local function execute_under_cursor()
-  local _6_ = request["request-from-cursor"]()
-  if (_6_ == nil) then
+  local _12_ = request["request-from-cursor"]()
+  if (_12_ == nil) then
     return api.nvim_err_writeln("Failed to create request")
-  elseif (nil ~= _6_) then
-    local req = _6_
+  elseif (nil ~= _12_) then
+    local req = _12_
     local buf = buffer["get-or-create-tmp"](buffer_name)
     local header = make_result_header(req)
     buffer["highlight-range"](vim.api.nvim_get_current_buf(), api.nvim_create_namespace("grpc-nvim"), req.start, req["end"], 500)
